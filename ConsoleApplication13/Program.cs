b9f46be93158f5b659bd32023e9b2d1552a22dec
raghuma sama <raghuma sama>
2016-10-27 22:59:38 -0400

    first commit

---
+++ ConsoleApplication13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static int sumtotal(int x,int y)
+        {
+          int  sumtotal = x + y;
+            return sumtotal;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(sumtotal(7, 8));
+        }
+    }
+}
